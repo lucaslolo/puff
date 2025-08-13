e0fcabb5b6d65b4989016138da0a74f9e451f1be
tshoney <lucaslolo1312@gmail.com>
2025-08-13 22:21:18 +0200

    triple monstre

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-// ============================= Loader Fade =============================
 window.addEventListener("load", () => {
+  // Loader
   const loader = document.getElementById("loader");
-  loader.style.opacity = "0"; // fade out
-  setTimeout(() => loader.style.display = "none", 500); // cache après la transition
+  loader.style.opacity = "0";
+  setTimeout(() => loader.style.display = "none", 500);
+
+  // Carousel NFT
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const nftGrid = document.querySelector(".grid-nfts");
+const scrollAmount = 220;
+
+leftArrow.addEventListener("click", () => {
+  nftGrid.scrollBy({ left: -scrollAmount, behavior: "smooth" });
 });
+
+rightArrow.addEventListener("click", () => {
+  nftGrid.scrollBy({ left: scrollAmount, behavior: "smooth" });
+});
+});
+
+
+
