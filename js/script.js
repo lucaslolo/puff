527e7263567940a9c3e3e0acdddcc95656cfad60
tshoney <lucaslolo1312@gmail.com>
2025-08-14 20:24:02 +0200

    triple monstre

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
   loader.style.opacity = "0"; // fade out
   setTimeout(() => loader.style.display = "none", 500); // cache après la transition
+
+
+  fetch('stock.json')
+    .then(response => response.json())
+    .then(stock => {
+      // Exemple pour Watermelon
+      document.getElementById('stock-number-watermelon').textContent = stock.watermelon;
+
+      // Si tu veux faire pareil pour les autres goûts
+      document.getElementById('stock-number-blueberry').textContent = stock.blueberry;
+      document.getElementById('stock-number-blackberry').textContent = stock.blackberry;
+      document.getElementById('stock-number-strawberry').textContent = stock.strawberry;
+      document.getElementById('stock-number-triplemelon').textContent = stock.triplemelon;
+    })
+    .catch(err => console.error('Erreur de chargement du stock:', err));
+
 });
